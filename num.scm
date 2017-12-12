ffe3ec1972650dc9c6999d993365a3f6cc19e64f
吴磊 <wuleiaty@users.noreply.github.com>
2017-12-12 20:14:44 +0800

    12/12

--- num.scm
+++ num.scm
@@ -38,9 +38,52 @@
 (define tup+
   (lambda (tup1 tup2)
     (cond
-      ((and (null? tup1) (null? tup2))
-       '())
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
       (else
        (cons (o+ (car tup1) (car tup2))
              (tup+ (cdr tup1) (cdr tup2)))))))
 
+(define o>
+  (lambda (m n)
+    (cond
+      ((zero? n) #t)
+      ((zero? m) #f)
+      (else
+       (o> (sub1 m) (sub1 n))))))
+
+(define o<
+  (lambda (m n)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else
+       (o< (sub1 m) (sub1 n))))))
+
+(define o^
+  (lambda (m n)
+    (cond
+      ((zero? n) 1)
+      (else
+       (o* m (o^ m (sub1 n)))))))
+
+(define o/
+  (lambda (m n)
+    (cond
+      ((o< m n) 0)
+      (else
+       (add1 (o/ (o- m n) n))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else
+       (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (cdr lat))
+      (else
+       (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
